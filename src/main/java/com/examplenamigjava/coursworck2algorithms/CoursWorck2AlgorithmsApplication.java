27b956cc2228ee9ec331fa3ee548cafd3805cc74
Namig1991 <safarovnamig09@gmail.com>
2022-05-16 11:39:04 +0300

    Мой первый коммит

---
+++ src/main/java/com/examplenamigjava/coursworck2algorithms/CoursWorck2AlgorithmsApplication.java
@@ -0,0 +1,13 @@
+package com.examplenamigjava.coursworck2algorithms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursWorck2AlgorithmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursWorck2AlgorithmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplenamigjava/coursworck2algorithms/CoursWorck2AlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplenamigjava.coursworck2algorithms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursWorck2AlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
